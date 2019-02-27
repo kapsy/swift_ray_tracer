007eaabcb057436253c559e198b2574dfd1b1efb
Kapsy <Michael.J.Petrie@starttoday.jp>
2019-02-27 18:58:59 +0900

    Implemented sphere intersections, displaying normals.

--- code/swift_cmd_line_test.swift
+++ code/swift_cmd_line_test.swift
@@ -8,7 +8,7 @@ func assertNaN(_ value: Float) {
     assert(!value.isNaN)
 }
 
-func frand48() -> Float {
+func drand48f() -> Float {
     return Float(drand48())
 }
 
@@ -16,14 +16,16 @@ func frand48() -> Float {
 
 struct V3
 {
-    var x: Float
-    var y: Float
-    var z: Float
+    var x = Float(0)
+    var y = Float(0)
+    var z = Float(0)
 
     // NOTE: (Kapsy) For C union like behavior.
     var r: Float { get { return x } }
     var g: Float { get { return y } }
     var b: Float { get { return z } }
+
+    init() {}
 
     init(_ val: Float) {
         self.x = val
@@ -108,34 +110,33 @@ func unit(_ a: V3) -> V3 {
     return res
 }
 
+// MARK: Static arrays
+
+
+struct Sphere {
+    var center = V3(0)
+    var rad = Float(0)
+    //material mat;
+}
+
+var MAX_SPHERES = Int(1 << 6)
+var spheres = [Sphere](repeating: Sphere(), count: MAX_SPHERES)
+var sphereCount = 0
+
 // MARK: Camera
 
 struct Camera {
 
-    var origin: V3
-    var lowerLeft: V3
-    var horiz: V3
-    var vert: V3
+    var origin = V3(0)
+    var lowerLeft = V3(0)
+    var horiz = V3(0)
+    var vert = V3(0)
 
-    var w: V3
-    var u: V3
-    var v: V3
+    var w = V3(0)
+    var u = V3(0)
+    var v = V3(0)
 
-    var lensRad: Float
-
-
-    init() {
-        self.origin = V3(0)
-        self.lowerLeft = V3(0)
-        self.horiz = V3(0)
-        self.vert = V3(0)
-
-        self.w = V3(0)
-        self.u = V3(0)
-        self.v = V3(0)
-
-        self.lensRad = Float(0)
-    }
+    var lensRad = Float(0)
 }
 
 
@@ -161,7 +162,7 @@ func getRay(_ c: inout Camera, _ s: Float, _ t: Float) -> Ray {
     // NOTE: (Kapsy) Random in unit disk.
     var rand = V3(0)
     repeat  {
-        rand = 2.0*V3(frand48(), frand48(), 0) - V3(1,1,0)
+        rand = 2.0*V3(drand48f(), drand48f(), 0) - V3(1,1,0)
     } while dot(rand, rand) >= 1.0
 
     let rd = c.lensRad*rand;
@@ -171,11 +172,124 @@ func getRay(_ c: inout Camera, _ s: Float, _ t: Float) -> Ray {
     return res
 }
 
-func getColor(_ ray: inout Ray, _ depth: Int) -> V3 {
-    return V3(1,0,0)
+func pointAt(_ ray: inout Ray, _ t: Float) -> V3 {
+    let res = ray.A + t*ray.B
+    return res
 }
 
+// NOTE: (Kapsy) Ray-sphere intersection:
+//
+// x^2 + y^2 + z^2 = R^2
+//
+// A.A = R^2
+// (A - C).(A - C) = R^2
+//
+// since A = A + tB
+//
+// (A + tB - C).(A + tB - C) = R^2
+//
+// v =def A - C
+//
+// starting another way:
+// in vector notation:
+//
+// (x - c)^2 = r^2
+// sub x = A + tB
+// (A + tB - C)^2 = r^2
+//
+// def v = A - C
+//
+// |v + tB|^2 = r^2
+//
+// (v + tB)*(v + tB)
+//
+// v^2 + vtB + vtB + t^2B^2
+// v^2 + 2v.tB + t^2B^2
+//
+// solve for:
+//
+// t^2(B^2) + t(2v.B) + (v^2 - r^2) = 0
+//
+
+//    a.b = |a||b|cos(?)
+//
+//
+//    a.a = |a||a|
+//    a.a = |a|^2
+
+// |v + tB|^2 = r^2
+// (v + tB).(v + tB)
+// v.(v + tB) + tB.(v + tB)
+// v.v + v.tB + tB.v + tB.tB
+// v.v + 2v.tB + t^2B.B
+//
+//
+// t*t*B.B + 2*t*B.v + v.v - R*R = 0
+//
+// |v|^2 + 2v.tB + t^2B^2
+
+func getColor(_ ray: inout Ray, _ depth: Int) -> V3 {
+
+    var res = V3()
+    let tmin = Float(0.001)
+    var tmax = Float.greatestFiniteMagnitude
+
+    var collision = false
+    var N = V3()
+    var p = V3()
+
+    for i in 0..<sphereCount {
+
+        let rad = spheres[i].rad
+        let center = spheres[i].center
+
+        let oc = ray.orig - center
+
+        let a = dot(ray.dir, ray.dir)
+        let b = dot(oc, ray.dir)
+        let c = dot(oc, oc) - rad*rad
+        let discriminant = b*b - a*c;
+
+        if discriminant > 0.0 {
+            var t = (-b - sqrt(discriminant))/a
+            if (tmin < t && t < tmax)
+            {
+                tmax = t
+                p = pointAt(&ray, t)
+                N = (p - center)/rad
+                // matIndex = i
+                collision = true
+            }
+
+            t = (-b + sqrt(discriminant))/a
+            if (tmin < t && t < tmax)
+            {
+                tmax = t
+                p = pointAt(&ray, t)
+                N = (p - center)/rad
+                // matIndex = i
+                collision = true
+            }
+
+        }
+    }
+
+    if (collision) {
+        res = 0.5*V3 (N.x+1, N.y+1, N.z+1);
+    } else {
+        res = V3(0,0,1)
+    }
+
+    return res
+}
+
+
 func main() {
+
+    var s0 = sphereCount
+    spheres[s0].center = V3(0, 0, -1)
+    spheres[s0].rad = 0.5
+    sphereCount += 1
 
 
     let frameRate = Float(25)
@@ -192,9 +306,9 @@ func main() {
 
         var data = Data()
 
-        let nx = Int(200)
-        let ny = Int(100)
-        let ns = Int(1)
+        let nx = Int(600)
+        let ny = Int(300)
+        let ns = Int(100)
 
         // NOTE: (Kapsy) Camera setup stuff.
 
@@ -235,8 +349,8 @@ func main() {
 
                 for s in 0..<ns {
 
-                    let u = (Float(i) + frand48())/Float(nx)
-                    let v = (Float(j) + frand48())/Float(ny)
+                    let u = (Float(i) + drand48f())/Float(nx)
+                    let v = (Float(j) + drand48f())/Float(ny)
 
                     var r = getRay(&cam, u, v)
 
